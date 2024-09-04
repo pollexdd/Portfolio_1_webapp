179e4a513eea02f101740d40e10952c3d5014513
pollekillerxd <76852143+pollexdd@users.noreply.github.com>
2024-09-04 19:04:54 +0200

    Initial commit

---
+++ my-app/README.md
@@ -0,0 +1,16 @@
+Får å starte seerver.
+```
+npm install
+npm run dev
+```
+
+```
+open http://localhost:3000
+```
+
+
+
+
+
+Wireframen ser litt forkjellig ut en det ferdige produktet. 
+Jeg byttet farger til det jeg synes så bedre ut og endret litt på utsene.
---
+++ my-app/public/scripts/fetchProjects.ts
@@ -0,0 +1,25 @@
+fetch('./projects.json')
+    .then(res => res.json())
+    .then(data => {
+        const projects = data.projects;
+        const projectList = document.querySelector('.project-list');
+        if (!projectList) return;
+
+        projectList.innerHTML = '';
+
+        projects.forEach((project: { name: string; description: string; technologies: string; }) => {
+            const projectElement = document.createElement('li');
+            projectElement.classList.add('project');
+
+            projectElement.innerHTML = `
+                <h2>${project.name}</h2>
+                <p>${project.description}</p>
+                <p>Technologies: ${project.technologies}</p>
+            `;
+
+            projectList.appendChild(projectElement);
+        });
+    })
+    .catch(error => {
+        console.error('There was a problem with the fetch operation:', error);
+    });
